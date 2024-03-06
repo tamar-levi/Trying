f54422acf1af2d32c476fc39c7b32b30826ce21b
shani-fisher <131307430+shani-fisher@users.noreply.github.com>
2024-03-06 02:24:35 +0200

    python shani

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+import  random;
+random.seed(34556);
+import  numpy as np;
+np.random.seed(778777);
+a=np.array([1,2,3,4,5,6],int);
+b=np.arange(0,6,1);
+c=np.random.randint(5,10,6)
+print(a)
+print(b)
+print(c)
+print(b*c);
+print(np.argmax(a))
+a=a.reshape(2,3);
+print(a.mean(axis=1))
+print(np.logical_and(a>1,a<5))
+print(a[np.logical_and(a>1,a<5)])
+a=np.where(np.logical_and(a>1,a<5),a**a,10);
+print(a)
+d=np.random.randint(100,200,21).reshape(3,7);
+print(d)
